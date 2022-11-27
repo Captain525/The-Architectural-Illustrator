994b68fddff3fcc7ad15addc76805cc5630bd70e
Captain525 <dheffren@rocketmail.com>
2022-11-27 12:28:43 -0500

    nearly ready to start running the model

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from loadData import loadDataMultiprocessing
 from EdgeDetection import createSketch, createSketchMultiprocessing
 import numpy as np
 import time
+import matplotlib.pyplot as plt
+from runModel import runModel
 if __name__ == "__main__":
     startLoad = time.time()
     imageTensor = loadDataMultiprocessing()
@@ -9,6 +11,10 @@ if __name__ == "__main__":
     loadTime = endLoad - startLoad
     #multiprocessing sketches is slower. 
     sketches = createSketch(imageTensor)
+    for i in range(10):
+        plt.imshow(sketches[i])
+        plt.show()
+    model = runModel(imageTensor, sketches)
     
     
     
--- runModel.py
+++ runModel.py
@@ -1,8 +1,27 @@
 import tensorflow as tf
 from GAN import GAN
+import numpy as np
+from skimage.util import img_as_float
+def split(images, sketches):
+    percentTrain = .8
+    numImages = images.shape[0]
+    numTrain = int(percentTrain*numImages)
+    indices = np.random.permutation(numImages)
+    mixedImages = img_as_float(images[indices])
+    mixedSketches = img_as_float(sketches[indices])
+    trainImages = images[:numTrain]
+    trainSketches = sketches[:numTrain]
+
+    testImages = images[numTrain:]
+    testSketches = sketches[numTrain:]
+    return trainImages, trainSketches, testImages, testSketches
 
 
-def runModel():
+
+def runModel(images, sketches):
+    #maybe use a faster library method for this instead. 
+    trainImages, trainSketches, testImages, testSketches = split(images, sketches)
+    
     learningRate = .0002
     b1 = .5
     b2 = .999
@@ -17,4 +36,5 @@ def runModel():
 
     model.compile(optimizerGen, optimizerDis, lossFxn, lossFxn)
 
-    model.fit()
+    model.fit(trainImages, trainSketches, batch_size = batchSize, epochs = epochs, validation_data = (testImages, testSketches))
+    
